01b0c5a075108c2438ec4a4ca5bc5e87ce35c935
Kento Watanabe <kento621@gmail.com>
2017-03-15 22:16:32 +0900

    Init

---
+++ SerialPlotter/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace SerialPlotter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.BTN_CON = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CMB_BAUD = new System.Windows.Forms.ComboBox();
+            this.CMB_COM = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.CMB_DATA = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.CMB_AVE = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart4 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Enabled = false;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 62);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.IsVisibleInLegend = false;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(738, 102);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // BTN_CON
+            // 
+            this.BTN_CON.Location = new System.Drawing.Point(273, 19);
+            this.BTN_CON.Name = "BTN_CON";
+            this.BTN_CON.Size = new System.Drawing.Size(118, 20);
+            this.BTN_CON.TabIndex = 1;
+            this.BTN_CON.Text = "CONNECT";
+            this.BTN_CON.UseVisualStyleBackColor = true;
+            this.BTN_CON.Click += new System.EventHandler(this.BTN_CON_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.CMB_BAUD);
+            this.groupBox1.Controls.Add(this.CMB_COM);
+            this.groupBox1.Controls.Add(this.BTN_CON);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(400, 44);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "COM Setting";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(129, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Baud Rate";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "COM";
+            // 
+            // CMB_BAUD
+            // 
+            this.CMB_BAUD.FormattingEnabled = true;
+            this.CMB_BAUD.Location = new System.Drawing.Point(194, 18);
+            this.CMB_BAUD.Name = "CMB_BAUD";
+            this.CMB_BAUD.Size = new System.Drawing.Size(73, 20);
+            this.CMB_BAUD.TabIndex = 6;
+            // 
+            // CMB_COM
+            // 
+            this.CMB_COM.FormattingEnabled = true;
+            this.CMB_COM.Location = new System.Drawing.Point(45, 19);
+            this.CMB_COM.Name = "CMB_COM";
+            this.CMB_COM.Size = new System.Drawing.Size(78, 20);
+            this.CMB_COM.TabIndex = 5;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.checkBox1);
+            this.groupBox2.Controls.Add(this.CMB_DATA);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.CMB_AVE);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(418, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(332, 44);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Graph Setting";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(277, 20);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(49, 16);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Save";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // CMB_DATA
+            // 
+            this.CMB_DATA.FormattingEnabled = true;
+            this.CMB_DATA.Location = new System.Drawing.Point(185, 18);
+            this.CMB_DATA.Name = "CMB_DATA";
+            this.CMB_DATA.Size = new System.Drawing.Size(86, 20);
+            this.CMB_DATA.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(150, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Data";
+            // 
+            // CMB_AVE
+            // 
+            this.CMB_AVE.FormattingEnabled = true;
+            this.CMB_AVE.Location = new System.Drawing.Point(58, 18);
+            this.CMB_AVE.Name = "CMB_AVE";
+            this.CMB_AVE.Size = new System.Drawing.Size(86, 20);
+            this.CMB_AVE.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Average";
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Enabled = false;
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(12, 170);
+            this.chart2.Name = "chart2";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.IsVisibleInLegend = false;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(738, 102);
+            this.chart2.TabIndex = 4;
+            this.chart2.Text = "chart2";
+            // 
+            // chart3
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea3);
+            legend3.Enabled = false;
+            legend3.Name = "Legend1";
+            this.chart3.Legends.Add(legend3);
+            this.chart3.Location = new System.Drawing.Point(12, 278);
+            this.chart3.Name = "chart3";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.IsVisibleInLegend = false;
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart3.Series.Add(series3);
+            this.chart3.Size = new System.Drawing.Size(738, 102);
+            this.chart3.TabIndex = 5;
+            this.chart3.Text = "chart3";
+            // 
+            // chart4
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart4.ChartAreas.Add(chartArea4);
+            legend4.Enabled = false;
+            legend4.Name = "Legend1";
+            this.chart4.Legends.Add(legend4);
+            this.chart4.Location = new System.Drawing.Point(12, 386);
+            this.chart4.Name = "chart4";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.IsVisibleInLegend = false;
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart4.Series.Add(series4);
+            this.chart4.Size = new System.Drawing.Size(738, 102);
+            this.chart4.TabIndex = 6;
+            this.chart4.Text = "chart4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(762, 508);
+            this.Controls.Add(this.chart4);
+            this.Controls.Add(this.chart3);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Serial Plotter v1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button BTN_CON;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox CMB_COM;
+        private System.Windows.Forms.ComboBox CMB_BAUD;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox CMB_AVE;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox CMB_DATA;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart4;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ SerialPlotter/Form1.cs
@@ -0,0 +1,424 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace SerialPlotter
+{
+    public partial class Form1 : Form
+    {
+
+        int MAX_HISTORY = 30;
+        int AVERAGE = 1;
+        
+        Queue<double> Qdata1 = new Queue<double>();
+        Queue<double> Qdata2 = new Queue<double>();
+        Queue<double> Qdata3 = new Queue<double>();
+        Queue<double> Qdata4 = new Queue<double>();
+        string filename;
+        string exePath;
+
+        int count;
+        double sum1, sum2, sum3, sum4;
+        
+        
+
+        private class BuadItem : Object
+        {
+            private string name = "";
+            private int val = 0;
+            
+            public string NAME
+            {
+                set { name = value; }
+                get { return name; }
+            }    
+        
+            public int BAUDRATE
+            {
+                set { val = value; }
+                get { return val; }
+            }
+           
+            public override string ToString()
+            {
+                return name;
+            }
+        }
+
+        private void showChart(Chart chart, Queue<int>  Qdata)
+        {
+
+            chart.Series[0].Points.Clear();
+            foreach (int value in Qdata)
+            {
+                chart.Series[0].Points.Add(new DataPoint(0, value));
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BTN_CON_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen == true)
+            {
+
+                try
+                {
+                    System.Windows.Forms.Application.DoEvents();
+                    if (true == serialPort1.IsOpen) serialPort1.DiscardInBuffer();
+                    System.Windows.Forms.Application.DoEvents();
+                    if (true == serialPort1.IsOpen) serialPort1.Close();
+                    CMB_BAUD.Enabled = true;
+                    CMB_COM.Enabled = true;
+                    System.Windows.Forms.Application.DoEvents();
+                    count = 1;
+
+
+
+                    BeginInvoke(new Delegate_ChangeButton(ChangeButton), new Object[] { "Connect" });
+
+                   
+                   
+                }
+                catch (Exception ex)
+                {
+                    //MessageBox.Show(ex.Message);
+                }
+
+            }
+            else {
+
+                serialPort1.PortName = CMB_COM.SelectedItem.ToString();
+				
+                BuadItem baud = (BuadItem)CMB_BAUD.SelectedItem;
+				serialPort1.BaudRate = baud.BAUDRATE;				
+				serialPort1.DataBits = 8;				
+				serialPort1.Parity = Parity.None;				
+				serialPort1.StopBits = StopBits.One;	
+                serialPort1.Handshake = Handshake.None;				
+				serialPort1.Encoding = Encoding.ASCII;
+                CMB_BAUD.Enabled = false;
+                CMB_COM.Enabled = false;
+                count = 1;
+
+                /*Get File Name*/
+                exePath = System.IO.Directory.GetCurrentDirectory();
+                DateTime dt = DateTime.Now;
+                filename = dt.Year.ToString("0000") + dt.Month.ToString("00") + dt.Day.ToString("00")
+                                  + dt.Hour.ToString("00") + dt.Minute.ToString("00") + dt.Second.ToString("00")+".csv";
+
+                try
+                {
+                    serialPort1.Open();
+                    if (true == serialPort1.IsOpen) serialPort1.DiscardInBuffer();
+                    BeginInvoke(new Delegate_ChangeButton(ChangeButton), new Object[] { "Disconnect" });
+                  
+
+                    
+                   
+                }
+                catch (Exception ex)
+                {
+                    //MessageBox.Show(ex.Message);
+                }
+            }
+
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+          
+
+
+            BeginInvoke(new Delegate_GetDataText(GetDataText), new Object[] { CMB_DATA });
+            BeginInvoke(new Delegate_GetAveText(GetAveText), new Object[] { CMB_AVE });
+
+
+            string[] StrArryData = new string[10];
+
+
+            if( serialPort1.IsOpen == false )	return;
+
+
+            try {
+                string comdata =  serialPort1.ReadLine();//ReadExisting()                
+                StrArryData = comdata.Split(new string[] { "," }, StringSplitOptions.None);
+
+                if (checkBox1.Checked)
+                {
+                             
+
+
+                    string csvPath = exePath + "\\" + filename;
+                    System.Text.Encoding enc =
+                        System.Text.Encoding.GetEncoding("Shift_JIS");
+                    System.IO.StreamWriter sr =
+                        new System.IO.StreamWriter(csvPath, true, enc);
+                    sr.WriteLine(comdata);
+                    sr.Close();
+
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message);
+            }
+
+           
+
+           
+
+
+            double tmp0=0.0, tmp1=0.0, tmp2=0.0, tmp3=0.0;
+            if (StrArryData.Length > 0) double.TryParse(StrArryData[0], out tmp0);
+            if (StrArryData.Length > 1) double.TryParse(StrArryData[1], out tmp1);
+            if (StrArryData.Length > 2) double.TryParse(StrArryData[2], out tmp2);
+            if (StrArryData.Length > 3) double.TryParse(StrArryData[3], out tmp3);
+
+            
+            
+            if (count < AVERAGE)
+            {
+                sum1 = sum1 + tmp0;
+                sum2 = sum2 + tmp1;
+                sum3 = sum3 + tmp2;
+                sum4 = sum4 + tmp3;
+
+                count++;
+            }
+            else {
+                
+                sum1 = sum1 + tmp0;
+                sum2 = sum2 + tmp1;
+                sum3 = sum3 + tmp2;
+                sum4 = sum4 + tmp3;
+
+               
+                Qdata1.Enqueue(sum1 / (double)AVERAGE);
+                Qdata2.Enqueue(sum2 / (double)AVERAGE);
+                Qdata3.Enqueue(sum3 / (double)AVERAGE);
+                Qdata4.Enqueue(sum4 / (double)AVERAGE);
+                sum1 = 0;
+                sum2 = 0;
+                sum3 = 0;
+                sum4 = 0;
+                count = 1;
+
+
+            }
+
+
+            while (Qdata1.Count > MAX_HISTORY) Qdata1.Dequeue();
+            while (Qdata2.Count > MAX_HISTORY) Qdata2.Dequeue();
+            while (Qdata3.Count > MAX_HISTORY) Qdata3.Dequeue();
+            while (Qdata4.Count > MAX_HISTORY) Qdata4.Dequeue();
+
+            BeginInvoke(new Delegate_ShowChart(ShowChart1), new Object[] { chart1 });
+            BeginInvoke(new Delegate_ShowChart(ShowChart2), new Object[] { chart2 });
+            BeginInvoke(new Delegate_ShowChart(ShowChart3), new Object[] { chart3 });
+            BeginInvoke(new Delegate_ShowChart(ShowChart4), new Object[] { chart4 });
+
+
+			
+        }
+
+
+        private delegate void Delegate_ShowChart(Chart chart);
+        private delegate void Delegate_GetDataText(Control CON);
+        private delegate void Delegate_GetAveText(Control CON);
+        private delegate void Delegate_ChangeButton(string text);
+
+
+        private void ShowChart1(Chart chart) {
+
+            
+            chart.Series[0].Points.Clear();
+            chart.Series[0].MarkerStyle = MarkerStyle.Circle;
+            chart.Series[0].MarkerSize = 6;
+            chart.Series[0].Color=Color.Blue;
+
+            if (Qdata1.Count > 2) {
+                chart.ChartAreas[0].AxisY.Minimum = (int)Qdata1.Min();
+               
+                
+            }
+           
+            foreach (int value in Qdata1)chart.Series[0].Points.Add(new DataPoint(0, value));
+        }
+
+
+
+
+
+        private void ShowChart2(Chart chart)
+        {
+
+
+            chart.Series[0].Points.Clear();
+            chart.Series[0].MarkerStyle = MarkerStyle.Circle;
+            chart.Series[0].MarkerSize = 6;
+            chart.Series[0].Color = Color.Green;
+
+            if (Qdata2.Count > 2)
+            {
+                chart.ChartAreas[0].AxisY.Minimum = (int)Qdata2.Min();
+             
+                
+            }
+
+            foreach (int value in Qdata2) chart.Series[0].Points.Add(new DataPoint(0, value));
+        }
+        private void ShowChart3(Chart chart)
+        {
+
+
+            chart.Series[0].Points.Clear();
+            chart.Series[0].MarkerStyle = MarkerStyle.Circle;
+            chart.Series[0].MarkerSize = 6;
+            chart.Series[0].Color = Color.Red;
+
+            if (Qdata3.Count > 2)
+            {
+                chart.ChartAreas[0].AxisY.Minimum = (int)Qdata3.Min();
+               
+                 
+            }
+
+            foreach (int value in Qdata3) chart.Series[0].Points.Add(new DataPoint(0, value));
+        }
+        private void ShowChart4(Chart chart)
+        {
+
+
+            chart.Series[0].Points.Clear();
+            chart.Series[0].MarkerStyle = MarkerStyle.Circle;
+            chart.Series[0].MarkerSize = 6;
+            chart.Series[0].Color = Color.Violet;
+
+            if (Qdata4.Count > 2)
+            {
+                chart.ChartAreas[0].AxisY.Minimum = (int)Qdata4.Min();
+               
+                
+            }
+
+            foreach (int value in Qdata4) chart.Series[0].Points.Add(new DataPoint(0, value));
+        }
+
+
+
+
+        private void GetDataText(Control CON)
+        {
+            int.TryParse(CON.Text, out MAX_HISTORY);
+            
+        }
+
+        private void GetAveText(Control CON)
+        {
+
+            int.TryParse(CON.Text, out AVERAGE);
+
+        }
+
+        private void ChangeButton(string text) {
+
+            BTN_CON.Text = text;
+        
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Init_Control();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            try
+            {
+                if (true == serialPort1.IsOpen) serialPort1.DiscardInBuffer();
+                System.Windows.Forms.Application.DoEvents();
+                if (serialPort1.IsOpen) serialPort1.Close();
+                System.Windows.Forms.Application.DoEvents();
+            }
+            catch (Exception ex)
+            {
+               // MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        private void Init_Control(){
+
+            /*Set COM Port List*/
+            string[] ports = SerialPort.GetPortNames();
+            CMB_COM.Items.Clear();
+            foreach (string prtname in ports)
+            {
+                CMB_COM.Items.Add(prtname);
+            }
+            if (CMB_COM.Items.Count > 0)
+            {
+                CMB_COM.SelectedIndex = CMB_COM.Items.Count - 1;
+            }
+
+            /*Set Baud Rate*/
+            BuadItem baud;
+            baud = new BuadItem();
+            baud.NAME = "4800bps";
+            baud.BAUDRATE = 4800;
+            CMB_BAUD.Items.Add(baud);
+
+            baud = new BuadItem();
+            baud.NAME = "9600bps";
+            baud.BAUDRATE = 9600;
+            CMB_BAUD.Items.Add(baud);
+
+            baud = new BuadItem();
+            baud.NAME = "19200bps";
+            baud.BAUDRATE = 19200;
+            CMB_BAUD.Items.Add(baud);
+
+            baud = new BuadItem();
+            baud.NAME = "115200bps";
+            baud.BAUDRATE = 115200;
+            CMB_BAUD.Items.Add(baud);
+            CMB_BAUD.SelectedIndex = 1;
+
+
+            /*Set Average List*/
+            CMB_AVE.Items.Add(1);
+            for (int i = 1; i < 11; i++)
+            {
+                CMB_AVE.Items.Add(2 * i);
+            }
+            CMB_AVE.SelectedIndex = 0;
+
+            /*Set Data List*/
+            CMB_DATA.Items.Add(1);
+            for (int i = 1; i < 11; i++)
+            {
+                CMB_DATA.Items.Add(i * 30);
+            }
+            CMB_DATA.SelectedIndex = 2;
+        }
+
+        public static int CountChar(string s, char c)
+        {
+            return s.Length - s.Replace(c.ToString(), "").Length;
+        }
+}
+}
